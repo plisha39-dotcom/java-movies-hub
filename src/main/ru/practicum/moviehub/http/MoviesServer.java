081f43e4e49a5f5b129f9a2d8801ed319fdab208
Plisha <mm3422kk@outlook.com>
2026-06-21 23:47:27 +0500

    добавил красный и зеленый тесты

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -1,5 +1,28 @@
 package ru.practicum.moviehub.http;
 
-public class MoviesServer {
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+class MoviesServer {
+    private final HttpServer server;
+
+    public MoviesServer() {
+        try {
+            server = HttpServer.create(new InetSocketAddress(8080), 0);
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось создать HTTP-сервер", e);
+        }
+    }
+
+    public void start() {
+        server.start();
+        System.out.println("Сервер запущен");
+    }
+
+    public void stop() {
+        server.stop(0);
+        System.out.println("Сервер остановлен");
+    }
 }
--- src/test/ru/practicum/moviehub/http/MoviesApiTest.java
+++ src/test/ru/practicum/moviehub/http/MoviesApiTest.java
@@ -5,6 +5,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MoviesApiTest {
 
     @BeforeAll
@@ -24,6 +34,34 @@ public class MoviesApiTest {
 
     @Test
     void getMovies_whenEmpty_returnsEmptyArray() throws Exception {
+        MoviesServer server = new MoviesServer();
 
+        server.start();
+
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .build();
+
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/movies"))
+                .GET()
+                .build();
+
+        HttpResponse.BodyHandler<String> responseBodyHandler =
+                HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
+        HttpResponse<String> resp = client.send(req, responseBodyHandler);
+
+        server.stop();
+
+        assertEquals(200, resp.statusCode(), "GET /movies должен вернуть 200");
+
+        String contentTypeHeaderValue =
+                resp.headers().firstValue("Content-Type").orElse("");
+        assertEquals("application/json; charset=UTF-8", contentTypeHeaderValue,
+                "Content-Type должен содержать формат данных и кодировку");
+
+        String body = resp.body().trim();
+        assertTrue(body.startsWith("[") && body.endsWith("]"),
+                "Ожидается JSON-массив");
     }
 }
